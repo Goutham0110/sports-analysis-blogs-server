fa89606cfe6a2af935444793974fed0cd45202ec
Goutham <82018550+Goutham0110@users.noreply.github.com>
2024-09-14 19:46:56 +0530

    Update configs.ts

--- src/config/configs.ts
+++ src/config/configs.ts
@@ -3,10 +3,10 @@ require('dotenv').config();
 const configs = {
     PORT: process.env.PORT ||  4000,
     MONGODB_CONNECTION_URL: process.env.MONGODB_CONNECTION_URL || "mongodb://localhost:27017/test",
-    NOTIFICATION_EMAIL: process.env.NOTIFICATION_EMAIL || "goutham.ec20@bitsathy.ac.in",
-    NOTIFICATION_EMAIL_PASSWORD: process.env.NOTIFICATION_EMAIL_PASSWORD || "Goutham@01",
+    NOTIFICATION_EMAIL: process.env.NOTIFICATION_EMAIL || "",
+    NOTIFICATION_EMAIL_PASSWORD: process.env.NOTIFICATION_EMAIL_PASSWORD || "",
     NOTIFICATION_EMAIL_SERVICE: process.env.NOTIFICATION_EMAIL_SERVICE || "gmail",
-    AUTHOR_EMAIL : process.env.AUTHOR_EMAIL || "gouthamsaravananr@gmail.com"
+    AUTHOR_EMAIL : process.env.AUTHOR_EMAIL || ""
 }
 
 export default configs;
